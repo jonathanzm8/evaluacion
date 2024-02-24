cd65845aed31e81c6c84e5aa85a1ae543335de5c
Joao <joaopriss@gmail.com>
2024-02-24 15:12:41 -0500

    Validacion apellido

--- integrante2/notas.js
+++ integrante2/notas.js
@@ -65,74 +65,129 @@ calcular = function (){
 
 }
 
-guardar = function() {
-    let nombre = recuperarTexto("txtNombre");
-    let apellido = recuperarTexto("txtApellido");
-    let  nota1 = recuperarFloat("txtNota1");
-    let nota2 = recuperarFloat("txtNota2");
-    let nota3 = recuperarFloat("txtNota3");
-    let nuevoEstudiante = {};
+guardar = function () {
+  let nombre = recuperarTexto("txtNombre");
+  let apellido = recuperarTexto("txtApellido");
+  let nota1 = recuperarFloat("txtNota1");
+  let nota2 = recuperarFloat("txtNota2");
+  let nota3 = recuperarFloat("txtNota3");
+  let nuevoEstudiante = {};
+
+  if (valiadarNombreApellidos(apellido, "lblError2")) {
     nuevoEstudiante.nombre = nombre;
     nuevoEstudiante.apellido = apellido;
     nuevoEstudiante.nota1 = nota1;
     nuevoEstudiante.nota2 = nota2;
     nuevoEstudiante.nota3 = nota3;
-    nuevoEstudiante.total = calcularTotal(nota1,nota2,nota3);
-    nuevoEstudiante.promedio = calcularPromedio(nota1,nota2,nota3).toFixed(2);
+    nuevoEstudiante.total = calcularTotal(nota1, nota2, nota3);
+    nuevoEstudiante.promedio = calcularPromedio(nota1, nota2, nota3).toFixed(2);
     //Validacion de campos vacios
     agregarEstudiante(nuevoEstudiante);
     limpiarCampo();
+  }
+};
 
-}
+agregarEstudiante = function (estudiante) {
+  estudiantes.push(estudiante);
+  mostrarEstudiantes();
+  alert(
+    "SE GUARDO CORRECTAMENTE EL ESTUDIANTE: " +
+      estudiante.nombre +
+      " " +
+      estudiante.apellido
+  );
+};
 
+mostrarEstudiantes = function () {
+  deshabilitarComponente("btnGuardar");
 
-agregarEstudiante=function(estudiante){
-    estudiantes.push(estudiante);
-    mostrarEstudiantes();
-    alert("SE GUARDO CORRECTAMENTE EL ESTUDIANTE: " + estudiante.nombre + " "+ estudiante.apellido);
+  let cmpTabla = document.getElementById("tablaEstudiantes");
+  let contenidoTabla =
+    "<table class='miTabla'><tr>" +
+    "<th>Nombre</th>" +
+    "<th>Apellido</th>" +
+    "<th>Nota 1</th>" +
+    "<th>Nota 2</th>" +
+    "<th>Nota 3</th>" +
+    "<th>Total</th>" +
+    "<th>Promedio</th>" +
+    "</tr>";
+  let estudiante;
+  for (let index = 0; index < estudiantes.length; index++) {
+    estudiante = estudiantes[index];
+    contenidoTabla +=
+      "<tr>" +
+      "<td>" +
+      estudiante.nombre +
+      "</td>" +
+      "<td>" +
+      estudiante.apellido +
+      "</td>" +
+      "<td>" +
+      estudiante.nota1 +
+      "</td>" +
+      "<td>" +
+      estudiante.nota2 +
+      "</td>" +
+      "<td>" +
+      estudiante.nota3 +
+      "</td>" +
+      "<td>" +
+      estudiante.total +
+      "</td>" +
+      "<td>" +
+      estudiante.promedio +
+      "</td>" +
+      "</tr>";
+  }
+  contenidoTabla += "</table>";
+  cmpTabla.innerHTML = contenidoTabla;
+};
 
-}
+limpiarCampo = function () {
+  mostrarTextoEnCaja("txtNombre", "");
+  mostrarTextoEnCaja("txtApellido", "");
+  mostrarTextoEnCaja("txtNota1", "");
+  mostrarTextoEnCaja("txtNota2", "");
+  mostrarTextoEnCaja("txtNota3", "");
+  mostrarTexto("lblResultadoTotal", 0.0);
+  mostrarTexto("lblResultadoPromedio", 0.0);
+  deshabilitarComponente("btnGuardar");
+};
 
-mostrarEstudiantes=function(){
-    deshabilitarComponente("btnGuardar");
+//validacion
+valiadarNombreApellidos = function (campo, componente) {
+  if (campo == "") {
+    mostrarTexto(componente, "*CAMPO OBLIGATORIO");
+    return false;
+  }
 
-    let cmpTabla = document.getElementById("tablaEstudiantes");
-    let contenidoTabla = "<table class='miTabla'><tr>" +
-        "<th>Nombre</th>" +
-        "<th>Apellido</th>" +
-        "<th>Nota 1</th>" +
-        "<th>Nota 2</th>" +
-        "<th>Nota 3</th>" +
-        "<th>Total</th>" +
-        "<th>Promedio</th>" +
-        "</tr>";
-    let estudiante;
-    for (let index = 0; index < estudiantes.length; index++) {
-        estudiante = estudiantes[index];
-        contenidoTabla += "<tr>" +
-            "<td>" + estudiante.nombre + "</td>" +
-            "<td>" + estudiante.apellido + "</td>" +
-            "<td>" + estudiante.nota1 + "</td>" +
-            "<td>" + estudiante.nota2 + "</td>" +
-            "<td>" + estudiante.nota3 + "</td>" +
-            "<td>" + estudiante.total + "</td>" +
-            "<td>" + estudiante.promedio + "</td>" +
+  if (campo.length >= 3) {
+    for (let i = 0; i < campo.length; i++) {
+      let element = campo[i];
 
-            "</tr>";
+      if (!esMayuscula(element)) {
+        mostrarTexto(componente, "Solo pueden contener letras mayusculas.");
+
+        return false;
+      }
     }
-    contenidoTabla += "</table>";
-    cmpTabla.innerHTML = contenidoTabla;
+  } else {
+    mostrarTexto(componente, "Deben tener al menos 3 caracteres.");
+    return false;
+  }
+  mostrarTexto(componente, "");
 
-}
+  return true;
+};
 
-limpiarCampo=function(){
-    mostrarTextoEnCaja("txtNombre", "");
-    mostrarTextoEnCaja("txtApellido", "");
-    mostrarTextoEnCaja("txtNota1", "");
-    mostrarTextoEnCaja("txtNota2", "");
-    mostrarTextoEnCaja("txtNota3", "");
-    mostrarTexto("lblResultadoTotal", 0.0);
-    mostrarTexto("lblResultadoPromedio", 0.0);
-    deshabilitarComponente("btnGuardar");
+esMayuscula = function (caracter) {
+  let code = caracter.charCodeAt();
 
-}
+  // Rango de códigos ASCII correspondiente a las letras mayúsculas A-Z
+  if (code >= 65 && code <= 90) {
+    return true;
+  } else {
+    return false;
+  }
+};
